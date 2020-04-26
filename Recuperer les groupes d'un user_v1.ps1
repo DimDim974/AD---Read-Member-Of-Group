0a998f07b327b8ef28e8aa195111f27cc9f0f35c
DimDim974 <dimitri.sautron@hotmail.fr>
2020-04-26 15:41:51 +0200

    Rename Recuperer les groupes d'un user.ps1 to Recuperer les groupes d'un user_v1.ps1

---
+++ Recuperer les groupes d'un user_v1.ps1
@@ -0,0 +1,8 @@
+﻿ Add-PSSnapin Quest.ActiveRoles.ADManagement
+$cp = read-host « Entre le nom d ouverture de session ADM  COMMUN\CP_ADM »
+$pw = read-host « Entre ton mot de passe ADM » -AsSecureString
+$DC = read-host « Entre le controleur de domaine »
+$co = Connect-QADService –service $DC –ConnectionAccount $cp  -ConnectionPassword $pwSelect
+$Agent = read-host « Entrer l agent »
+$user = Get-QADUser $Agent
+$user.memberOf | Get-QADGroup | Select-Object Name,DN | export-csv "D:\Aurele.csv" -encoding "utf8" -Delimiter ";"
